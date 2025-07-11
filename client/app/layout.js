31c5c438fc52c57ca02276935693e60d7f8d9fe2
abhishekjaswal <abhishekjaswal1122@gmail.com>
2025-07-11 14:09:48 +0530

    font family added

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,26 +1,21 @@
-
 'use client';
-import "./globals.css";
-import { Patrick_Hand } from "next/font/google";
-
-
-import { SessionProvider } from "next-auth/react";
+import './globals.css';
+import { Patrick_Hand } from 'next/font/google';
+import { SessionProvider } from 'next-auth/react';
 
 const patrickHand = Patrick_Hand({
-  weight: "400", // You can specify the weight, here it is 400 (regular)
-  style: "normal",
-  subsets: ["latin"],
-  variable: "--font-patrick-hand", // Custom variable to use across the app
+  weight: '400',
+  style: 'normal',
+  subsets: ['latin'],
+  variable: '--font-patrick-hand',
 });
-
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={patrickHand.variable}>
+      <body className="font-[var(--font-patrick-hand)]">
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
   );
 }
-
